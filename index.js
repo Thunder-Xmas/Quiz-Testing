10f1a4191400e5054873bc3a84421eed66fcc4e8
Thunder-Xmas <124792396+Thunder-Xmas@users.noreply.github.com>
2023-03-11 07:56:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const prefecturalCapital = [
       answer02: "福島",
       answer03: "前橋",
       answer04: "秋田",
-      answer05: "秋田",
+      answer05: "坂戸",
     },
     {
       id: "02",
@@ -17,7 +17,7 @@ const prefecturalCapital = [
       answer02: "前橋",
       answer03: "秋田",
       answer04: "札幌",
-      answer05: "秋田",
+      answer05: "坂戸",
     },
     {
       id: "03",
@@ -26,7 +26,7 @@ const prefecturalCapital = [
       answer02: "福島",
       answer03: "仙台",
       answer04: "山形",
-      answer05: "秋田",
+      answer05: "坂戸",
     },
     {
       id: "04",
@@ -35,7 +35,7 @@ const prefecturalCapital = [
       answer02: "札幌",
       answer03: "前橋",
       answer04: "水戸",
-      answer05: "秋田",
+      answer05: "坂戸",
     },
     {
       id: "05",
@@ -44,48 +44,189 @@ const prefecturalCapital = [
       answer02: "盛岡",
       answer03: "青森",
       answer04: "札幌",
-      answer05: "秋田",
+      answer05: "坂戸",
     },
   ];
 
-import java.util.*;
+let quiz_number = 5;
 
 let CurrentQuestionNo = 0;
 let point = 0;
-
+let step = 0; //0:start 1:quiz_now
+let quiz_shuffle = [0,1,2,3,4];
+let answer_shuffle = [0,1,2,3,4];
+let n2h;
+let buf;
+let last_answer = 0;
 
 function button1(){
-  /*
-  alert("Check A");*/
-  
-  ArrayList<Integer> list = new ArrayList<Integer>(0,1,2,3,4,5,6,7,8,9);
-  // listに値を入れる。この段階では昇順
-  // シャッフルして、順番を変える
-  Collections.shuffle(list);*/
+  if(step<(quiz_number+3) && step>1){
+    if(document.getElementById("quiz-text01").innerHTML == last_answer){
+      alert("正解！　「 "+ last_answer +" 」");
+      point+=1;
+    } 
+    if(document.getElementById("quiz-text01").innerHTML != last_answer){
+      alert("はずれ！　正解は 「 "+ last_answer +" 」");
+    } 
+  }
+  quizClicked();
+}
+function button2(){
+  if(step<(quiz_number+3) && step>1){
+    if(document.getElementById("quiz-text02").innerHTML == last_answer){
+      alert("正解！　「 "+ last_answer +" 」");
+      point+=1;
+    } 
+    if(document.getElementById("quiz-text02").innerHTML != last_answer){
+      alert("はずれ！　正解は 「 "+ last_answer +" 」");
+    } 
+  }
+  quizClicked();
+}
+function button3(){
+  if(step<(quiz_number+3) && step>1){
+    if(document.getElementById("quiz-text03").innerHTML == last_answer){
+      alert("正解！　「 "+ last_answer +" 」");
+      point+=1;
+    } 
+    if(document.getElementById("quiz-text03").innerHTML != last_answer){
+      alert("はずれ！　正解は 「 "+ last_answer +" 」");
+    } 
+  }
+  quizClicked();
+}
+function button4(){
+  if(step<(quiz_number+3) && step>1){
+    if(document.getElementById("quiz-text04").innerHTML == last_answer){
+      alert("正解！　「 "+ last_answer +" 」");
+      point+=1;
+    } 
+    if(document.getElementById("quiz-text04").innerHTML != last_answer){
+      alert("はずれ！　正解は 「 "+ last_answer +" 」");
+    } 
+  }
+  quizClicked();
+}
+function button5(){
+  if(step<(quiz_number+3) && step>1){
+    if(document.getElementById("quiz-text05").innerHTML == last_answer){
+      alert("正解！　「 "+ last_answer +" 」");
+      point+=1;
+    } 
+    if(document.getElementById("quiz-text05").innerHTML != last_answer){
+      alert("はずれ！　正解は 「 "+ last_answer +" 」");
+    } 
+  }
+  quizClicked();
+}
 
+function quizClicked(){
   
+  //alert("Check A");
   
-  let buf = document.getElementById("IDquiz-question-number");
-  buf.innerHTML = "問題" + prefecturalCapital[2].id;
-  CurrentQuestionNo = prefecturalCapital[2].id;
-  
-  buf = document.getElementById("IDquiz-question");
-  buf.innerHTML = prefecturalCapital[2].question;
-  
-  buf = document.getElementById("quiz-text01");
-  buf.innerHTML = prefecturalCapital[2].answer01;
-  
-  buf = document.getElementById("quiz-text02");
-  buf.innerHTML = prefecturalCapital[2].answer02;
-  
-  buf = document.getElementById("quiz-text03");
-  buf.innerHTML = prefecturalCapital[2].answer03;
-  
-  buf = document.getElementById("quiz-text04");
-  buf.innerHTML = prefecturalCapital[2].answer04;
+  if(step==0){
+    
+    for(let i=0; i<quiz_number; i++){
+      let random = Math.floor(Math.random() * (quiz_number));
+      let shuf_buf = 0;
+      shuf_buf = quiz_shuffle[random];
+      quiz_shuffle[random] = quiz_shuffle[i];
+      quiz_shuffle[i] = shuf_buf;
+    }
+    for(let i=0; i<5; i++){
+      let random = Math.floor(Math.random() * (5));
+      let shuf_buf = 0;
+      shuf_buf = answer_shuffle[random];
+      answer_shuffle[random] = answer_shuffle[i];
+      answer_shuffle[i] = shuf_buf;
+    }
+
+    step += 1;
+  }
   
 
+  if(step>quiz_number){
+    
+    buf = document.getElementById("IDquiz-question-number");
+    buf.innerHTML = "結果発表";
+
+    buf = document.getElementById("IDquiz-question");
+    if(point<quiz_number)buf.innerHTML = point+"問正解！";
+    if(point==quiz_number)buf.innerHTML = "全問正解！　おめでとう";
+    
+    
+    buf = document.getElementById("quiz-text01");
+    buf.innerHTML = "---";
+    buf = document.getElementById("quiz-text02");
+    buf.innerHTML = "---";
+    buf = document.getElementById("quiz-text03");
+    buf.innerHTML = "---";
+    buf = document.getElementById("quiz-text04");
+    buf.innerHTML = "---";
+    buf = document.getElementById("quiz-text05");
+    buf.innerHTML = "---";
+      
+    buf = document.getElementById("quiz-rw");
+    buf.innerHTML = "リロードしてもう一度挑戦！";
+
+    step+=1;
+    
+  }
+
+  if(step<=quiz_number){
+
+    for(let i=0; i<5; i++){
+      let random = Math.floor(Math.random() * (5));
+      let shuf_buf = 0;
+      shuf_buf = answer_shuffle[random];
+      answer_shuffle[random] = answer_shuffle[i];
+      answer_shuffle[i] = shuf_buf;
+    }
+    
+    buf = document.getElementById("IDquiz-question-number");
+    buf.innerHTML = "問題" + step;
+    CurrentQuestionNo = prefecturalCapital[quiz_shuffle[step-1]].id;
+    
+    buf = document.getElementById("IDquiz-question");
+    buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].question;
+    
+    buf = document.getElementById("quiz-text01");
+    n2h=answer_shuffle[0];
+    numToHTML();
+    
+    buf = document.getElementById("quiz-text02");
+    n2h=answer_shuffle[1];
+    numToHTML();
+    
+    buf = document.getElementById("quiz-text03");
+    n2h=answer_shuffle[2];
+    numToHTML();
+    
+    buf = document.getElementById("quiz-text04");
+    n2h=answer_shuffle[3];
+    numToHTML();
+    
+    buf = document.getElementById("quiz-text05");
+    n2h=answer_shuffle[4];
+    numToHTML();
+    
+    buf = document.getElementById("quiz-rw");
+    buf.innerHTML = quiz_shuffle[0]+", "+quiz_shuffle[1]+", "+quiz_shuffle[2]+", "+quiz_shuffle[3]+", "+quiz_shuffle[4];
+
+    last_answer = prefecturalCapital[quiz_shuffle[step-1]].answer01
+    
+    step+=1;
+    
+  }
   
+}
+
+function numToHTML(){
+  if(n2h==0)  buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].answer01;
+  if(n2h==1)  buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].answer02;
+  if(n2h==2)  buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].answer03;
+  if(n2h==3)  buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].answer04;
+  if(n2h==4)  buf.innerHTML = prefecturalCapital[quiz_shuffle[step-1]].answer05;
 
   
 }
